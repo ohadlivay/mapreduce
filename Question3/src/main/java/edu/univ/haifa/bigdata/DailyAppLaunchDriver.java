b67ba954535fa573c6f80cd9dcf033f4aae6ac83
Ohad Livay <156086151+ohadlivay@users.noreply.github.com>
2025-12-28 19:23:19 +0200

    test

--- Question3/src/main/java/edu/univ/haifa/bigdata/DailyAppLaunchDriver.java
+++ Question3/src/main/java/edu/univ/haifa/bigdata/DailyAppLaunchDriver.java
@@ -1,44 +1,102 @@
+/* Set package */
 package edu.univ.haifa.bigdata;
 
+/* Import libraries */
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
-public class DailyAppLaunchDriver {
+/* Driver class */
+public class DailyAppLaunchDriver extends Configured implements Tool {
 
-    public static void main(String[] args) throws Exception {
+    @Override
+    public int run(String[] args) throws Exception {
+        /* variables */
+        int exitCode;
+        Path inputPath;
+        Path outputPath;
+
+        // Check arguments consistency
         if (args.length != 2) {
             System.err.println("Usage: DailyAppLaunchDriver <input path> <output path>");
-            System.exit(-1);
+            return -1;
         }
 
-        Configuration conf = new Configuration();
-        Path inputPath = new Path(args[0]);
-        Path outputPath = new Path(args[1]);
+        // Parse parameters
+        inputPath = new Path(args[0]);
+        outputPath = new Path(args[1]);
 
-        // Ensure output directory does not exist
+        // Define and configure a new job
+        Configuration conf = this.getConf();
+
+        // Ensure output directory does not exist (retained from your original logic)
         FileSystem fs = FileSystem.get(conf);
         if (fs.exists(outputPath)) {
             fs.delete(outputPath, true);
         }
 
-        Job job = Job.getInstance(conf, "Daily App Launch Analysis");
+        Job job = Job.getInstance(conf);
+
+        // Assign a name to the job
+        job.setJobName("Daily App Launch Analysis");
+
+        // Set path of the input file/folder for this job
+        FileInputFormat.addInputPath(job, inputPath);
+
+        // Set path of the output folder for this job
+        FileOutputFormat.setOutputPath(job, outputPath);
+
+        // Set input format
+        // TextInputFormat = textual files (explicitly set as per lecture style)
+        job.setInputFormatClass(TextInputFormat.class);
+
+        // Set job output format
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        // Specify the class of the Driver for this job
         job.setJarByClass(DailyAppLaunchDriver.class);
 
+        // Set mapper class
         job.setMapperClass(DailyAppLaunchMapper.class);
+
+        // Set map output key and value classes
+        // (Lecture explicitly sets these even if they match the reducer output)
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
+
+        // Set reduce class
         job.setReducerClass(DailyAppLaunchReducer.class);
 
-        // Both Mapper and Reducer output Text keys and Text values
+        // Set reduce output key and value classes
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
 
-        FileInputFormat.addInputPath(job, inputPath);
-        FileOutputFormat.setOutputPath(job, outputPath);
+        // Set number of reducers
+        // (The lecture typically parses this from args, but here we use default or 1 as per original)
+        // job.setNumReduceTasks(numberOfReducers);
 
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+        // Execute the job and wait for completion
+        if (job.waitForCompletion(true) == true)
+            exitCode = 0;
+        else
+            exitCode = 1;
+
+        return exitCode;
+    }
+
+    /* main method of the driver class */
+    public static void main(String args[]) throws Exception {
+        /* Exploit the ToolRunner class to "configure" and run the Hadoop application */
+        int res = ToolRunner.run(new Configuration(), new DailyAppLaunchDriver(), args);
+        System.exit(res);
     }
 }
